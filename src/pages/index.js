bce82a9e333cf5c56ad8d3a7ce7ca2d54315fe96
zhangchuangye <1072491557@qq.com>
2021-06-01 11:37:54 +0800

    pages

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Description: 
+ * @Author: zhangchuangye
+ * @Date: 2021-06-01 11:34:07
+ */
+/*
+ * @Description: 
+ * @Author: zhangchuangye
+ * @Date: 2021-06-01 09:29:56
+ */
+// 伪代码 仅为了练习subtree
+function Dialog(type){
+    return  new this[type]()
+ }
+ 
+ Dialog.prototype.open=function(param){
+    this.state='open'
+ }
+ 
+ const pop=new Dialog('open',1)
+ console.log(pop.state)
+ 
